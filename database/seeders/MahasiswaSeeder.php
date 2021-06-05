8a433df080ba15fd413e141a5862d688c6e66eef
Julius634 <braindead936@gmail.com>
2021-06-05 13:57:14 +0700

    project

---
+++ database/seeders/MahasiswaSeeder.php
@@ -0,0 +1,23 @@
+<?php
+namespace Database\Seeders;
+use Illuminate\Database\Seeder;
+use App\Models\Mahasiswa;
+class MahasiswaSeeder extends Seeder
+{
+/**
+* Run the database seeds.
+*
+* @return void
+*/
+public function run()
+{
+$mhs1 = new Mahasiswa;
+$mhs1->nbi = "1461700234";
+$mhs1->nama_mhs = "Julius ferdinand";
+$mhs1->save();
+$mhs2 = new Mahasiswa;
+$mhs2->nbi = "1461800002";
+$mhs2->nama_mhs = "Arif Nur";
+$mhs2->save();
+}
+}
